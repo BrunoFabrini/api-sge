22f02d4741df17db518783e8c30179dc124f23ca
Bruno Fabrini <brunofabrini@outlook.com>
2022-03-29 22:30:14 -0300

    Conteinerização via Docker

--- Startup.cs
+++ Startup.cs
@@ -74,10 +74,10 @@ namespace api_sge
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api_sge v1"));
             }
-
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api_sge v1"));
             app.UseHttpsRedirection();
 
             app.UseRouting();
